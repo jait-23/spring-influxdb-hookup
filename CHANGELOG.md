9f9e92e18dba969e779e55c5796864a532819431
jait-23 <jait@si2tech.com>
2018-04-03 13:35:47 -0700

    InfluxDB connection files

---
+++ CHANGELOG.md
@@ -0,0 +1,60 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/).
+
+## [Unreleased]
+
+## [1.7] - 2018-02-18
+### Changed
+- Update to Spring Boot `1.5.10.RELEASE`
+- Updated to `influxdb-java` version `2.8`
+
+## [1.6] - 2017-08-03
+### Added
+- Added gzip support
+- Added chunking support for queries
+- Added possibility to configure  connect, read, and write timeouts
+### Changed
+- Update to Spring Boot `1.5.3.RELEASE`
+- Updated to latest `influxdb-java` version
+
+## [1.5] - 2017-01-18
+### Added
+- Added Docker Compose configuration to launch a InfluxDB test environment
+### Changed
+- Updated to latest `influxdb-java` version
+
+## [1.4] - 2016-07-06
+### Added
+- Added `DefaultInfluxDBTemplate` implementation to simply handle `Point` objects from `influxdb-java`
+
+## [1.3] - 2016-07-06
+### Added
+- Example application
+### Changed
+- Refactoring to use a "better" conversion strategy
+
+## [1.2] - 2016-06-14
+### Changed
+- Fixed typos
+
+## [1.1] - 2016-04-04
+### Added
+- Documentation and examples
+### Changed
+- Refactoring and improvements to use the template and its converters
+
+## 1.0 - 2016-04-02
+### Added
+- Basic Spring Data integration with the official `influxdb-java` library
+- Generic template (`InfluxDBTemplate`) to interact with InfluxDB
+
+[Unreleased]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.6...HEAD
+[1.7]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.6...spring-data-influxdb-1.7
+[1.6]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.5...spring-data-influxdb-1.6
+[1.5]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.4...spring-data-influxdb-1.5
+[1.4]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.3...spring-data-influxdb-1.4
+[1.3]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.2...spring-data-influxdb-1.3
+[1.2]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.1...spring-data-influxdb-1.2
+[1.1]: https://github.com/miwurster/spring-data-influxdb/compare/spring-data-influxdb-1.0...spring-data-influxdb-1.1
---
+++ src/test/java/org/springframework/data/influxdb/Application.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.influxdb;
+
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner
+{
+  private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+  @Autowired
+  private InfluxDBTemplate<Point> influxDBTemplate;
+
+  @Override
+  public void run(final String... args) throws Exception
+  {
+    // Create database...
+    influxDBTemplate.createDatabase();
+
+    // Create some data...
+    final Point p1 = Point.measurement("cpu")
+      .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+      .tag("tenant", "default")
+      .addField("idle", 90L)
+      .addField("user", 9L)
+      .addField("system", 1L)
+      .build();
+    final Point p2 = Point.measurement("disk")
+      .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+      .tag("tenant", "default")
+      .addField("used", 80L)
+      .addField("free", 1L)
+      .build();
+    influxDBTemplate.write(p1, p2);
+
+    // ... and query the latest data
+    final Query q = new Query("SELECT * FROM cpu GROUP BY tenant", influxDBTemplate.getDatabase());
+    influxDBTemplate.query(q, 10, queryResult -> logger.info(queryResult.toString()));
+  }
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Application.class, args);
+  }
+}
